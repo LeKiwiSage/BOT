e62bcecd9127d00148a0c705b6e734286b37f6a7
LeKiwiSage <163763939+LeKiwiSage@users.noreply.github.com>
2024-05-09 13:32:15 +0200

    Update PICTET_BOT.py

--- PICTET_BOT.py
+++ PICTET_BOT.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
+echo "backend: TkAgg" >> ~/.matplotlib/matplotlibrc
 import streamlit as st
 import numpy as np
+import matplotlib.pyplot as plt
 import pandas as pd
 
 st.title('P.I.C.T.E.T Portfolio Optimizer')
