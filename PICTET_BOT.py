383f64300c62a88a069e64579b42a36bf467e568
LeKiwiSage <163763939+LeKiwiSage@users.noreply.github.com>
2024-05-14 00:11:37 +0200

    Update PICTET_BOT.py

--- PICTET_BOT.py
+++ PICTET_BOT.py
@@ -165,7 +165,7 @@ def main():
         risk_capacity_category = determine_risk_capacity_category(risk_capacity)
         risk_profile_category = determine_risk_profile_category(risk_profile)
 
-        txt = st.markdown("""**The following risk profile has been prepared based on your risk capacity and risk tolerance:**""")
+        txt = st.markdown("""**<p class="questionnaire-header">The following risk profile has been prepared based on your risk capacity and risk tolerance:</p>**""")
         st.write("Risk Appetite:", risk_appetite_category)
         st.write("Risk Capacity:", risk_capacity_category)
         st.write("Risk Profile:", risk_profile_category)
